86f6a994649631a693608130cdf6828893d238c0
viegas <viegas@viegas>
2017-05-03 10:41:50 +0100

    Moore features definition

--- src/main/java/fcul/viegas/bigflow/dto/Features_MOORE_DTO.java
+++ src/main/java/fcul/viegas/bigflow/dto/Features_MOORE_DTO.java
@@ -14,5 +14,89 @@ public class Features_MOORE_DTO {
     private Integer destinationPort;
     private Integer sourcePort;
     
+    private Integer minimumInterArrivalTime;
+    private Integer quartileFirstInterArrivalTime;
+    private Integer medianInterArrivalTime;
+    private Integer avgInterArrivalTime;
+    private Integer quartileThirdInterArrivalTime;
+    private Integer maximumInterArrivalTime;
+    private Integer varianceInterArrivalTime;
+    
+    private Integer minimumInterArrivalTime_a_b;
+    private Integer quartileFirstInterArrivalTime_a_b;
+    private Integer medianInterArrivalTime_a_b;
+    private Integer avgInterArrivalTime_a_b;
+    private Integer quartileThirdInterArrivalTime_a_b;
+    private Integer maximumInterArrivalTime_a_b;
+    private Integer varianceInterArrivalTime_a_b;
+    
+    private Integer minimumInterArrivalTime_b_a;
+    private Integer quartileFirstInterArrivalTime_b_a;
+    private Integer medianInterArrivalTime_b_a;
+    private Integer avgInterArrivalTime_b_a;
+    private Integer quartileThirdInterArrivalTime_b_a;
+    private Integer maximumInterArrivalTime_b_a;
+    private Integer varianceInterArrivalTime_b_a;
+    
+    //for us dataWire = packetLength
+    private Integer minimumDataWire;
+    private Integer quartileFirstDataWire;
+    private Integer medianDataWire;
+    private Integer avgDataWire;
+    private Integer quartileDataWire;
+    private Integer maximumDataWire;
+    private Integer varianceDataWire;
+    
+    private Integer minimumDataWire_a_b;
+    private Integer quartileFirstDataWire_a_b;
+    private Integer medianDataWire_a_b;
+    private Integer avgDataWire_a_b;
+    private Integer quartileDataWire_a_b;
+    private Integer maximumDataWire_a_b;
+    private Integer varianceDataWire_a_b;
+    
+    private Integer minimumDataWire_b_a;
+    private Integer quartileFirstDataWire_b_a;
+    private Integer medianDataWire_b_a;
+    private Integer avgDataWire_b_a;
+    private Integer quartileDataWire_b_a;
+    private Integer maximumDataWire_b_a;
+    private Integer varianceDataWire_b_a;
+    
+    private Integer total_packets_a_b;
+    private Integer total_packets_b_a;
+    
+    private Integer ack_pkts_sent_a_b;
+    private Integer ack_pkts_sent_b_a;
+    
+    private Integer pure_acks_sent_a_b;
+    private Integer pure_acks_sent_b_a;
+
+    private Integer pushed_pkts_sent_a_b;
+    private Integer pushed_pkts_sent_b_a;
+    
+    private Integer syn_pkts_sent_a_b;
+    private Integer syn_pkts_sent_b_a;
+    
+    private Integer fin_pkts_sent_a_b;
+    private Integer fin_pkts_sent_b_a;
+    
+    private Integer urgent_pkts_sent_a_b;
+    private Integer urgent_pkts_sent_b_a;
+    
+    private Integer throughput_a_b;
+    private Integer throughput_b_a;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
