807ddf9001bacfc0ae6bb7589308fc0f616df0bf
viegas <viegas@viegas>
2017-05-08 10:53:18 +0100

    Feature Extractor implemented!

--- src/main/java/fcul/viegas/bigflow/Main.java
+++ src/main/java/fcul/viegas/bigflow/Main.java
@@ -7,21 +7,24 @@ package fcul.viegas.bigflow;
 
 import fcul.viegas.topologies.Topologies_ARFF_CREATOR;
 
-/**
- *
+/*
  * @author viegas
  */
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        
+        /*
         String param1 = "/home/viegas/Desktop/saida/201601101400.txt";
         String param2 = "/home/viegas/Desktop/saida/20160110_anomalous_suspicious.csv";
         String param3 = "/home/viegas/Desktop/saida/20160110.arff";
         
         Topologies_ARFF_CREATOR.runTopology(
                 param1, param2, param3);
-
+*/
+        Topologies_ARFF_CREATOR.runTopology(
+                args[0], 
+                args[1], 
+                args[2]);
     }
 
 }
