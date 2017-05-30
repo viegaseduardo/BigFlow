fca28fd3ee70b1648f3697b2b9339eed69c187c2
viegas <viegas@viegas>
2017-05-30 10:48:31 +0100

    implemented histogram feature

--- src/main/java/fcul/viegas/topologies/machinelearning/Topologies_SPARK_CREATE_CLUSTERS.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/Topologies_SPARK_CREATE_CLUSTERS.java
@@ -7,21 +7,30 @@ package fcul.viegas.topologies.machinelearning;
 
 import fcul.viegas.bigflow.definitions.Definitions;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.DoubleFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.ml.feature.MaxAbsScaler;
+import org.apache.spark.ml.feature.MaxAbsScalerModel;
+import org.apache.spark.ml.linalg.VectorUDT;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
-import org.apache.spark.mllib.tree.model.GradientBoostedTreesModel;
 import org.apache.spark.mllib.clustering.KMeans;
 import org.apache.spark.mllib.clustering.KMeansModel;
-import scala.Tuple2;
+import org.apache.spark.mllib.stat.MultivariateStatisticalSummary;
+import org.apache.spark.mllib.stat.Statistics;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 /**
  *
@@ -34,9 +43,11 @@ public class Topologies_SPARK_CREATE_CLUSTERS {
         Definitions.SPARK_FEATURE_SET = featureSet;
 
         SparkConf sparkConf = new SparkConf().setAppName("Topologies_SPARK_CREATE_CLUSTERS");
-        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+        SparkContext jsc = new SparkContext(sparkConf);
 
-        JavaRDD<String> fileArff = jsc.textFile(pathArff);
+        SparkSession spark = new SparkSession(jsc);
+
+        JavaRDD<String> fileArff = jsc.textFile(pathArff, 1).toJavaRDD();
 
         JavaRDD<LabeledPoint> inputData = fileArff.map(new Function<String, LabeledPoint>() {
             @Override
@@ -79,7 +90,30 @@ public class Topologies_SPARK_CREATE_CLUSTERS {
             }
         });
 
-        JavaRDD<LabeledPoint> inputDataNormal = inputData.filter(new Function<LabeledPoint, Boolean>() {
+        MultivariateStatisticalSummary summary = Statistics.colStats(inputData.map(new Function<LabeledPoint, Vector>() {
+            @Override
+            public Vector call(LabeledPoint t1) throws Exception {
+                return t1.features();
+            }
+        }).rdd());
+
+        JavaRDD<LabeledPoint> inputDataNormalized = inputData.map(new Function<LabeledPoint, LabeledPoint>() {
+            @Override
+            public LabeledPoint call(LabeledPoint t1) throws Exception {
+
+                double[] feats = t1.features().toArray();
+
+                for (int i = 0; i < feats.length; i++) {
+                    feats[i] = (feats[i] - summary.min().toArray()[i]) / (summary.max().toArray()[i] - summary.min().toArray()[i])
+                            * 2 - 1;
+                }
+
+                return t1;
+            }
+        });
+
+        
+        JavaRDD<LabeledPoint> inputDataNormal = inputDataNormalized.filter(new Function<LabeledPoint, Boolean>() {
             @Override
             public Boolean call(LabeledPoint t1) throws Exception {
                 return t1.label() == 0.0d;
@@ -92,22 +126,156 @@ public class Topologies_SPARK_CREATE_CLUSTERS {
                 return t1.features();
             }
         });
+        
+        
+        JavaRDD<LabeledPoint> inputDataSuspicious = inputDataNormalized.filter(new Function<LabeledPoint, Boolean>() {
+            @Override
+            public Boolean call(LabeledPoint t1) throws Exception {
+                return t1.label() == 2.0d;
+            }
+        });
+
+        JavaRDD<Vector> vecSuspicious = inputDataSuspicious.map(new Function<LabeledPoint, Vector>() {
+            @Override
+            public Vector call(LabeledPoint t1) throws Exception {
+                return t1.features();
+            }
+        });
+        
+        
+        JavaRDD<LabeledPoint> inputDataAnomalous = inputDataNormalized.filter(new Function<LabeledPoint, Boolean>() {
+            @Override
+            public Boolean call(LabeledPoint t1) throws Exception {
+                return t1.label() == 1.0d;
+            }
+        });
+
+        JavaRDD<Vector> vecAnomalous = inputDataAnomalous.map(new Function<LabeledPoint, Vector>() {
+            @Override
+            public Vector call(LabeledPoint t1) throws Exception {
+                return t1.features();
+            }
+        });
+        
 
         int numClusters = numberOfClusters;
         int numIterations = 20;
         KMeansModel clusters = KMeans.train(vecNormal.rdd(), numClusters, numIterations);
 
+        Vector center = clusters.clusterCenters()[0];
+        
+        
+        JavaRDD<Double> vecDistanceNormal = vecNormal.map(new Function<Vector, Double>() {
+            @Override
+            public Double call(Vector t1) throws Exception {
+                double[] vecClusterDouble = center.toArray();
+                double[] vect1 = t1.toArray();
+                double diff_square_sum = 0.0;
+                for (int i = 0; i < vecClusterDouble.length; i++) {
+                    diff_square_sum += (vecClusterDouble[i] - vect1[i]) * (vecClusterDouble[i] - vect1[i]);
+                }
+                return Math.sqrt(diff_square_sum);
+            }
+        });
+
+        JavaDoubleRDD vecDoubleNormal = vecDistanceNormal.mapToDouble(new DoubleFunction<Double>() {
+            @Override
+            public double call(Double t) throws Exception {
+                return t;
+            }
+        });
+
+        JavaRDD<Double> vecDistanceSuspicious = vecSuspicious.map(new Function<Vector, Double>() {
+            @Override
+            public Double call(Vector t1) throws Exception {
+                double[] vecClusterDouble = center.toArray();
+                double[] vect1 = t1.toArray();
+                double diff_square_sum = 0.0;
+                for (int i = 0; i < vecClusterDouble.length; i++) {
+                    diff_square_sum += (vecClusterDouble[i] - vect1[i]) * (vecClusterDouble[i] - vect1[i]);
+                }
+                return Math.sqrt(diff_square_sum);
+            }
+        });
+
+        JavaDoubleRDD vecDoubleSuspicious = vecDistanceSuspicious.mapToDouble(new DoubleFunction<Double>() {
+            @Override
+            public double call(Double t) throws Exception {
+                return t;
+            }
+        });
+
+        JavaRDD<Double> vecDistanceAnomalous = vecAnomalous.map(new Function<Vector, Double>() {
+            @Override
+            public Double call(Vector t1) throws Exception {
+                double[] vecClusterDouble = center.toArray();
+                double[] vect1 = t1.toArray();
+                double diff_square_sum = 0.0;
+                for (int i = 0; i < vecClusterDouble.length; i++) {
+                    diff_square_sum += (vecClusterDouble[i] - vect1[i]) * (vecClusterDouble[i] - vect1[i]);
+                }
+                return Math.sqrt(diff_square_sum);
+            }
+        });
+
+        JavaDoubleRDD vecDoubleAnomalous = vecDistanceAnomalous.mapToDouble(new DoubleFunction<Double>() {
+            @Override
+            public double call(Double t) throws Exception {
+                return t;
+            }
+        });
+
+        double minNormal = vecDoubleNormal.min();
+        double maxNormal = vecDoubleNormal.max();
+        double minSuspicious = vecDoubleSuspicious.min();
+        double maxSuspicious = vecDoubleSuspicious.max();
+        double minAnomalous = vecDoubleAnomalous.min();
+        double maxAnomalous = vecDoubleAnomalous.max();
+
+        double[] buckets = new double[1000];
+
         PrintWriter writer = new PrintWriter(outputPath, "UTF-8");
 
-        for (Vector center : clusters.clusterCenters()) {
-            double vec[] = center.toArray();
-            for (double d : vec) {
-                writer.print(d + ",");
-            }
-            writer.println("normal");
+        double increment = minNormal;
+        for (int i = 0; i < 1000; i++) {
+            buckets[i] = increment;
+            increment += (maxNormal - minNormal) / 1000.0f;
         }
-        
+        long[] normalHistogram = vecDoubleNormal.histogram(buckets);
+
+        writer.println("Normal avg: " + vecDoubleNormal.mean() + " min: " + minNormal + " max: " + maxNormal);
+
+        for (int i = 0; i < buckets.length - 1; i++) {
+            writer.printf("[%f-%f];%d\n", buckets[i], buckets[i + 1], normalHistogram[i]);
+        }
+
+        increment = minSuspicious;
+        for (int i = 0; i < 1000; i++) {
+            buckets[i] = increment;
+            increment += (maxSuspicious - minSuspicious) / 1000.0f;
+        }
+        long[] suspiciousHistogram = vecDoubleSuspicious.histogram(buckets);
+        writer.println("Suspicious avg: " + vecDoubleSuspicious.mean() + " min: " + minSuspicious + " max: " + maxSuspicious);
+
+        for (int i = 0; i < buckets.length - 1; i++) {
+            writer.printf("[%f-%f];%d\n", buckets[i], buckets[i + 1], suspiciousHistogram[i]);
+        }
+
+        increment = minAnomalous;
+        for (int i = 0; i < 1000; i++) {
+            buckets[i] = increment;
+            increment += (maxAnomalous - minAnomalous) / 1000.0f;
+        }
+        long[] anomalousHistogram = vecDoubleAnomalous.histogram(buckets);
+        writer.println("Normal avg: " + vecDoubleAnomalous.mean() + " min: " + minAnomalous + " max: " + maxAnomalous);
+
+        for (int i = 0; i < buckets.length - 1; i++) {
+            writer.printf("[%f-%f];%d\n", buckets[i], buckets[i + 1], anomalousHistogram[i]);
+        }
+
         writer.close();
+
+        
 
     }
 
