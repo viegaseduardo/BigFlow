dd64dc5126f0ab227553e97991ebea1e71b1b062
viegas <viegas@viegas>
2017-06-12 10:33:26 +0100

    testing

--- src/main/java/fcul/viegas/topologies/machinelearning/Topologies_WEKA_Tests_WithUpdateThreaded.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/Topologies_WEKA_Tests_WithUpdateThreaded.java
@@ -424,7 +424,7 @@ public class Topologies_WEKA_Tests_WithUpdateThreaded extends Thread {
 
                 //newDataTrainNewMonth = this.selectFeatures(newDataTrainNewMonth);
                 //System.out.println(newDataTrainNewMonth.size());
-                classifier = this.trainClassifierNaive(newDataTrainNewMonth);
+                classifier = this.trainClassifierTree(newDataTrainNewMonth);
 
             }
 
