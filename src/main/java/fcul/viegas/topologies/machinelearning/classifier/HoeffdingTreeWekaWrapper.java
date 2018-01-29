f880d4d09483b00f38f4a5bed7feca0bfc9dc4c8
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-01-29 14:24:42 -0200

    more fixes

--- src/main/java/fcul/viegas/topologies/machinelearning/classifier/HoeffdingTreeWekaWrapper.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/classifier/HoeffdingTreeWekaWrapper.java
@@ -5,16 +5,19 @@ import com.yahoo.labs.samoa.instances.SamoaToWekaInstanceConverter;
 import moa.classifiers.AbstractClassifier;
 import moa.core.Measurement;
 import weka.classifiers.trees.HoeffdingTree;
+import weka.core.Instances;
 
 public class HoeffdingTreeWekaWrapper extends AbstractClassifier{
 
     private HoeffdingTree hoeff;
+    private boolean isInitialized;
     private SamoaToWekaInstanceConverter converter;
 
     public HoeffdingTreeWekaWrapper() {
         super();
         this.converter = new SamoaToWekaInstanceConverter();
         this.hoeff = new HoeffdingTree();
+        this.isInitialized = false;
     }
 
     @Override
@@ -32,12 +35,17 @@ public class HoeffdingTreeWekaWrapper extends AbstractClassifier{
     public void resetLearningImpl() {
         this.hoeff = new HoeffdingTree();
         this.converter = new SamoaToWekaInstanceConverter();
+        this.isInitialized = false;
     }
 
     @Override
     public void trainOnInstanceImpl(Instance instance) {
         weka.core.Instance instWeka = this.converter.wekaInstance(instance);
         try {
+            if(!this.isInitialized){
+                this.hoeff.buildClassifier(this.converter.wekaInstancesInformation(instance.dataset()));
+                this.isInitialized = true;
+            }
             this.hoeff.updateClassifier(instWeka);
         }catch(Exception ex){
             ex.printStackTrace();
