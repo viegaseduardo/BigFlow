3e871b8485fa9206c93e96caa706370b98a4436f
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-03-27 11:38:30 -0300

    more fixes

--- src/main/java/fcul/viegas/topologies/machinelearning/Topologies_MOA_ConformalThresholdFinder.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/Topologies_MOA_ConformalThresholdFinder.java
@@ -550,15 +550,15 @@ public class Topologies_MOA_ConformalThresholdFinder {
 
                 String[] split = line.split(";");
 
-                float error = Float.valueOf(split[3]) + Float.valueOf(split[4]);
-                error = error / 2.0f;
-                float rejection = ((Float.valueOf(split[10]) + Float.valueOf(split[11]))/2.0f);
-                rejection = 1.0f - rejection;
-
-                //float error = Float.valueOf(split[4]);
-                //error = error / 1.0f;
-                //float rejection = ((Float.valueOf(split[11]))/1.0f);
+                //float error = Float.valueOf(split[3]) + Float.valueOf(split[4]);
+                //error = error / 2.0f;
+                //float rejection = ((Float.valueOf(split[10]) + Float.valueOf(split[11]))/2.0f);
                 //rejection = 1.0f - rejection;
+
+                float error = Float.valueOf(split[4]);
+                error = error / 1.0f;
+                float rejection = ((Float.valueOf(split[11]))/1.0f);
+                rejection = 1.0f - rejection;
 
                 NonDominated nonDominated = new NonDominated();
                 nonDominated.error = error;
