f1dc18fbaaf59e07d16bef50e6091594bb59a3d3
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-03-27 16:38:12 -0300

    more fixes

--- src/main/java/fcul/viegas/topologies/machinelearning/Topologies_MOA_ConformalThresholdFinder.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/Topologies_MOA_ConformalThresholdFinder.java
@@ -344,11 +344,11 @@ public class Topologies_MOA_ConformalThresholdFinder {
 
         for(ValueForRejectEvaluation values: listValuesAll){
             values.averageAttackProb = 0.0d;
-            values.averageNormalProb = 0.0d;
+            values.averageNormalProb = 1.0d;
             for(int k = 0; k < wekaWrapper.getMoaClassifiers().size(); k++){
                 if(values.predictClassClassifier.get(k) == 0.0d){
                     double normalizedProb = values.alphaEachClassifier.get(k)[1] / maxProbClassifier.get(k);
-                    if(normalizedProb < values.averageNormalProb || values.averageNormalProb == 0.0d){
+                    if(normalizedProb < values.averageNormalProb || values.averageNormalProb == 1.0d){
                         values.averageNormalProb = normalizedProb;
                     }
                 }else{
