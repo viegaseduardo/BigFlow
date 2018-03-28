d22f7b53693fd1d13f77a3a3f41db7b7fb62b0c4
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-03-28 18:46:23 -0300

    more fixes`

--- src/main/java/fcul/viegas/topologies/machinelearning/MachineLearningModelBuilders.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/MachineLearningModelBuilders.java
@@ -501,7 +501,7 @@ public class MachineLearningModelBuilders implements Serializable {
                 "gracePeriod",
                 'g',
                 "The number of instances a leaf should observe between split attempts.",
-                100, 0, Integer.MAX_VALUE);
+                200, 0, Integer.MAX_VALUE);
         classifier.leafpredictionOption = new MultiChoiceOption(
                 "leafprediction", 'l', "Leaf prediction to use.", new String[]{
                 "MC", "NB", "NBAdaptive"}, new String[]{
@@ -509,14 +509,14 @@ public class MachineLearningModelBuilders implements Serializable {
                 "Naive Bayes",
                 "Naive Bayes Adaptive"}, 2);
 
-       // ClassBalancer balancer = new ClassBalancer();
-      //  balancer.setInputFormat(train);
-      //  Instances newTrain = Filter.useFilter(train, balancer);
+        ClassBalancer balancer = new ClassBalancer();
+        balancer.setInputFormat(train);
+        Instances newTrain = Filter.useFilter(train, balancer);
 
-        SpreadSubsample subsample = new SpreadSubsample();
-        subsample.setInputFormat(train);
-        subsample.setDistributionSpread(1.0d);
-        Instances newTrain = Filter.useFilter(train, subsample);
+        //SpreadSubsample subsample = new SpreadSubsample();
+        //subsample.setInputFormat(train);
+        //subsample.setDistributionSpread(1.0d);
+        //Instances newTrain = Filter.useFilter(train, subsample);
 
         Randomize randomTrain = new Randomize();
         randomTrain.setInputFormat(newTrain);
