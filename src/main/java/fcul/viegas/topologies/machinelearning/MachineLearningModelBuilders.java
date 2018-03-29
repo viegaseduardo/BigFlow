b780760871686f6d37d81249f61bda4db9ee2352
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-03-29 01:49:04 -0300

    more fixes`

--- src/main/java/fcul/viegas/topologies/machinelearning/MachineLearningModelBuilders.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/MachineLearningModelBuilders.java
@@ -502,7 +502,7 @@ public class MachineLearningModelBuilders implements Serializable {
                 "gracePeriod",
                 'g',
                 "The number of instances a leaf should observe between split attempts.",
-                1000, 0, Integer.MAX_VALUE);
+                500, 0, Integer.MAX_VALUE);
      /*   classifier.leafpredictionOption = new MultiChoiceOption(
                 "leafprediction", 'l', "Leaf prediction to use.", new String[]{
                 "MC", "NB", "NBAdaptive"}, new String[]{
