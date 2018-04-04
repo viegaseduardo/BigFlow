b955a435309a4c482e892e2b2f1063e4ff70d370
eduardoviegas <eduardo.viegas@ppgia.pucpr.br>
2018-04-04 10:43:59 -0300

    more fixes`

--- src/main/java/fcul/viegas/topologies/machinelearning/ConformalEvaluator/ConformalEvaluator_Batch_ThresholdFinder.java
+++ src/main/java/fcul/viegas/topologies/machinelearning/ConformalEvaluator/ConformalEvaluator_Batch_ThresholdFinder.java
@@ -125,7 +125,7 @@ public class ConformalEvaluator_Batch_ThresholdFinder {
 
         System.out.println("building conformal");
         ConformalEvaluator_Batch conformalEvaluator = new ConformalEvaluator_Batch();
-        conformalEvaluator.buildConformal(dataTrain);
+        //conformalEvaluator.buildConformal(dataTrain);
 
 
         System.out.println("building classifiers now, this will take some time.........");
@@ -213,10 +213,12 @@ public class ConformalEvaluator_Batch_ThresholdFinder {
                 values.instClass = inst.classValue();
                 values.predictClass = predict;
                 if(values.predictClass == 0.0d){
-                    values.alpha = conformalEvaluator.getPValueForNormal(inst);
+                    //values.alpha = conformalEvaluator.getPValueForNormal(inst);
+                    values.alpha = classifier.distributionForInstance(inst)[0];
                     listValuesPredictedNormal.add(values);
                 }else{
-                    values.alpha = conformalEvaluator.getPValueForAttack(inst);
+                    //values.alpha = conformalEvaluator.getPValueForAttack(inst);
+                    values.alpha = classifier.distributionForInstance(inst)[1];
                     listValuesPredictedAttack.add(values);
                 }
 
@@ -335,9 +337,6 @@ public class ConformalEvaluator_Batch_ThresholdFinder {
                         if (n == 0) {
                             n = 1;
                         }
-
-
-
 
 
 
